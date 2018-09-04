5b31439d250e24e9f0a6453b26a5d1f4973a10c6
llius <llius619@gmail.com>
2018-09-04 12:25:58 +0200

    1/3 Buscador facturas
    1/3 Hecho del buscador facturas.

--- src/app/factura/factura.component.ts
+++ src/app/factura/factura.component.ts
@@ -1,19 +1,22 @@
-import { ModeloFactura } from './../models/model.service';
-import { map } from 'rxjs/operators';
-import { Component, OnInit } from '@angular/core';
-import { SqlFactura } from '../sql/sq.factura.service';
-import { Router, ActivatedRoute } from '../../../node_modules/@angular/router';
-import { FacturaDesplegada } from '../models/factura-desplegada.service';
-
+import { ModeloFactura } from "./../models/model.service";
+import { map } from "rxjs/operators";
+import { Component, OnInit } from "@angular/core";
+import { SqlFactura } from "../sql/sq.factura.service";
+import { Router, ActivatedRoute } from "../../../node_modules/@angular/router";
+import { FacturaDesplegada } from "../models/factura-desplegada.service";
 
 @Component({
-  selector: 'app-factura',
-  templateUrl: './factura.component.html',
-  styleUrls: ['./factura.component.css']
+  selector: "app-factura",
+  templateUrl: "./factura.component.html",
+  styleUrls: ["./factura.component.css"]
 })
 export class FacturaComponent implements OnInit {
-
-  constructor(private sqlFactura: SqlFactura, private router: Router, private route: ActivatedRoute, private facturaDesplegada: FacturaDesplegada) { }
+  constructor(
+    private sqlFactura: SqlFactura,
+    private router: Router,
+    private route: ActivatedRoute,
+    private facturaDesplegada: FacturaDesplegada
+  ) {}
 
   facturas: ModeloFactura;
   factura2: ModeloFactura;
@@ -22,18 +25,22 @@ export class FacturaComponent implements OnInit {
   arrayTipoFactura = [];
 
   ngOnInit() {
-    this.sqlFactura.todasFacturas()
+    this.sqlFactura
+      .todasFacturas()
       .pipe(
         map((data: any) => {
-          this.cargarFacturas(data)
+          this.cargarFacturas(data);
         })
-      ).subscribe();
-    this.sqlFactura.todosLosTipos()
+      )
+      .subscribe();
+    this.sqlFactura
+      .todosLosTipos()
       .pipe(
-        map((data) => {
+        map(data => {
           this.cargarTipoFacturas(data);
         })
-      ).subscribe()
+      )
+      .subscribe();
   }
 
   cargarFacturas(factura: any) {
@@ -44,16 +51,38 @@ export class FacturaComponent implements OnInit {
   }
 
   tipoFactura(index: number) {
-    index = index -1;
+    index = index - 1;
     return this.arrayTipoFactura[index];
   }
 
   factura_desplegada(factura: ModeloFactura) {
-    
     this.factura2 = factura;
     this.factura2.tipo_id = this.tipoFactura(this.factura2.tipo_id);
     this.facturaDesplegada.setFactura(this.factura2);
-    this.router.navigate(['factura-desplegada'], { relativeTo: this.route})
+    this.router.navigate(["factura-desplegada"], { relativeTo: this.route });
   }
 
+  tipoFacturaBuscar: boolean = false;
+  tipoFechaBuscar: boolean = false;
+
+  seleccion(valor: string) {
+    switch (valor) {
+      case "factura":
+        this.tipoFacturaBuscar = true;
+        this.tipoFechaBuscar = false;
+        break;
+      case "fecha":
+        this.tipoFacturaBuscar = false;
+        this.tipoFechaBuscar = true;
+        break;
+      default:
+        this.tipoFacturaBuscar = false;
+        this.tipoFechaBuscar = false;
+        break;
+    }
+  }
+
+  buscar(tipo: any) {
+    console.log(tipo);
+  }
 }
