90353e2e61fe9b948cc120f7205d0f6545305435
llius <llius619@gmail.com>
2018-09-06 12:27:00 +0200

    Incidencias 1/3
    Añadido los inputs.

--- src/app/incidencias/incidencias.component.ts
+++ src/app/incidencias/incidencias.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormControlName, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-incidencias',
-  templateUrl: './incidencias.component.html',
-  styleUrls: ['./incidencias.component.css']
+  selector: "app-incidencias",
+  templateUrl: "./incidencias.component.html",
+  styleUrls: ["./incidencias.component.css"]
 })
 export class IncidenciasComponent implements OnInit {
+  formControlIncidencia = new FormGroup({
+    formControlInputPuerta: new FormControl(),
+    formControlInputEmail: new FormControl(),
+    formControlInputTelefono: new FormControl(),
+    formControlInputDescripcion: new FormControl()
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  enviar() {}
+  limpiar() {
+    this.formControlIncidencia.reset()
   }
-
 }
